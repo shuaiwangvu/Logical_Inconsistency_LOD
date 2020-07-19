8de8443c550136a10c85e7e4d7981f887cb9c85f
shuaiwangvu <shuai.wang.vu@gmail.com>
2020-07-19 11:34:48 +0200

    extra

--- analysis_rdf_schema_lod.py
+++ analysis_rdf_schema_lod.py
@@ -46,3 +46,5 @@ for s in collect_nodes:
 print('# collect triple in LOD: ', len (collect_triple_rdf_lod))
 collect_extra = collect_triple_rdf_lod.difference(collect_triple_rdf)
 print ('# extra: ', len(collect_extra))
+for (s, p, o) in collect_extra:
+    print ('They are:' s, p, o)

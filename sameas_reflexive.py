ad7701441df6def9024e75fccafb61172ef9ceba
shuaiwangvu <shuai.wang.vu@gmail.com>
2020-07-20 16:23:16 +0200

    obtain statistics sameas

--- sameas_reflexive.py
+++ sameas_reflexive.py
@@ -4,6 +4,7 @@ import rdflib
 from hdt import HDTDocument, IdentifierPosition
 from collections import Counter
 import difflib
+import tldextract
 
 PATH_LOD = "/scratch/wbeek/data/LOD-a-lot/data.hdt"
 hdt_file =  HDTDocument(PATH_LOD)
@@ -16,6 +17,19 @@ narrower = "http://www.w3.org/2004/02/skos/core#narrower"
 hasPart = "http://purl.org/dc/terms/hasPart"
 
 predicate_list = [hasPart, narrower, broader, subClassOf, equivalent, sameas]
+
+
+
+
+def get_domain_and_label(t):
+    domain = tldextract.extract(t).domain
+    name1 = t.rsplit('/', 1)[-1]
+    name2 = t.rsplit('#', 1)[-1]
+    if len(name2) < len(name1):
+        return (domain, name2)
+    else:
+        return (domain, name1)
+
 
 def find_how_many(predicate):
     print('For predicate: ', predicate)
@@ -33,5 +47,30 @@ def find_how_many(predicate):
     print ('Among them, there are in total ', len (collect_nodes), ' reflexive arrows')
     print ('There are ', (len(collect_nodes)/cardinality)*100, 'percent reflexive arrows')
 
-for p in predicate_list:
-    find_how_many(p)
+# for p in predicate_list:
+#     find_how_many(p)
+
+# find out the domain and count them:
+(triples, cardinality) = hdt_file.search_triples("", sameas, "")
+ct_domain = Counter()
+ct_name = Counter()
+# ct[] += 1
+count = 0
+for (s, p, o) in triples:
+    if s == o:
+        count += 1
+        if count % (int (cardinality/10)) == 0:
+            print ('progress: ', count /cardinality)
+        domain, name = get_domain_and_label(s)
+        ct_domain[domain] += 1
+        ct_name[name] += 1
+
+
+for n in ct_domain:
+    if ct_domain[n] > 10:
+        print ('DOMAIN: ', ct_domain[n])
+
+
+for n in ct_name:
+    if ct_name[n] > 5:
+        print ('NAME: ', ct_name[n])
